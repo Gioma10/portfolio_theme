15690e0cedd87f8e961d3b71aad37a55bea16389
Giovanni Mauro <giovi.mauro03@gmail.com>
2024-02-05 17:00:34 +0100

    creazione e importazione file su laravel con inserimento vite per collegare gli stili e gli script

---
+++ laravel_portfolio/resources/js/index.js
@@ -0,0 +1,384 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+//!NAVBAR
+// navbar link
+gsap.set(".nav-link", {
+y: -500,
+});
+gsap.to(".nav-link", {
+y: 0,
+duration: 0.7,
+stagger: 0.1,
+ease: "power1.out",
+});
+// icon social
+gsap.set(".fa-brands-navbar", {
+fontSize: 22,
+y: -500,
+x: -50,
+});
+gsap.to(".fa-brands-navbar", {
+y: 0,
+stagger: 0.1,
+ease: "power1.out",
+duration: 2,
+});
+
+// click link navbar
+const changeColor= document.querySelectorAll('.click-color');
+let linkNavbar= 5;
+for(let i=0;i<linkNavbar;i++){
+    changeColor[i].addEventListener('click',()=>{
+        gsap.to(changeColor, {
+            color:'grey',
+            duration:0.1,
+        })
+        gsap.to(changeColor[i], {
+            color:'white',
+            duration:0.1,
+        })
+    });
+}
+
+
+//! HEADER
+// apetura e chiusura banner hero
+gsap.set('.banner-hero', {
+    x:-1530,
+})
+const arrowHero= document.querySelector('.arrow-hero');
+let arrowValidation= true;
+// evento click freccettina
+arrowHero.addEventListener('click',()=>{
+    
+    if(arrowValidation){
+        // apertura banner
+        gsap.to('.banner-hero', {
+            x:-700,
+            duration:1,
+            ease:'power3.inOut',
+        })
+        // arrow animation open 
+        gsap.to(arrowHero, {
+            rotate:540,
+            repeat:1,
+        })
+        arrowValidation=false;
+        // bowling loghi
+        gsap.to('.logo_pro_custom', {
+            x:500,
+            y:'random(200,-200)',
+            rotate:'random(20,360)',
+            delay:0.5,
+        })
+        // banner image animation open
+        gsap.to('.banner-img', {
+            x: -200,
+            duration:1,
+            delay:0.7,
+            ease:'power1.out'
+        })
+        // banner text animation open
+        gsap.to('.banner-text', {
+            x: 200,
+            duration:1,
+            delay:0.7,
+            ease:'power1.out'
+        })
+    }else{
+        // chiusura banner
+        gsap.to('.banner-hero', {
+            x:-1530,
+            duration:1,
+            ease:'power3.inOut'
+        })
+        // arrow animation close
+        gsap.to(arrowHero, {
+            rotate:0,
+            repeat:1,
+        })
+        arrowValidation=true;
+        // ritorno loghi
+        gsap.to('.logo_pro_custom', {
+            x:0,
+            y:0,
+            rotate:0,
+            delay:0.5,
+            duration:2,
+            ease:'power3.in'
+        })
+        // banner image animation close
+        gsap.to('.banner-img', {
+            x: 0,
+            duration:0.5,
+            ease:'power1.out'
+        })
+        // banner text animation close
+        gsap.to('.banner-text', {
+            x: 0,
+            duration:0.5,
+            ease:'power1.out'
+        })
+    }
+ 
+    
+})
+
+
+//! SECTION ABOUT ME
+
+//* Animazione gsap
+// const aboutMe= document.querySelector('#aboutMe');
+
+// // animazione titolo section 1
+// function s1TitleAnimate(){
+//     gsap.set('#about-title', {
+//         y:0,
+//         x:800,
+//     })
+//     gsap.to('#about-title', {
+//         y:180,
+//         duration:3,
+//         ease:'power3.out'
+//     })
+
+// }
+
+// // animazione immagine section 1
+// function s1ImgAnimate(){
+
+//     gsap.set('.about-img', {
+//         y:130,
+//         autoAlpha:0,
+//     })
+//     gsap.to('.about-img', {
+//         autoAlpha:1,
+//         x:300,
+//         duration:3,
+//         ease:'power3.out',
+//     })
+// }
+// // animazione testo section 1
+// function s1TextAnimate(){
+
+//     gsap.set('.about-text', {
+//         y:230,
+//         x:-100,
+//         autoAlpha:0,
+//     })
+//     gsap.to('.about-text', {
+//         autoAlpha:1,
+//         x:-200,
+//         duration:3,
+//         ease:'power3.out',
+//     })
+// }
+
+// // observe
+// let observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entrie) => {
+//         console.log(entrie);
+//         if (entrie.isIntersecting) {
+//             s1TitleAnimate();
+//             s1ImgAnimate();
+//             s1TextAnimate();
+//         console.log(entrie);
+//         }
+//     })
+// })
+// observer.observe(aboutMe);
+
+
+
+
+
+//! SECTION WORK
+gsap.registerPlugin(ScrollTrigger);
+
+// let sections = gsap.utils.toArray(".panel");
+
+// gsap.to(sections, {
+//     xPercent: -100 * (sections.length - 1),
+//     ease: "ease",
+//     scrollTrigger: {
+//         marker:true,
+//       trigger: ".s2",
+//       pin: true,
+//       scrub: 1,
+//       snap: {
+//         snapTo: 1 / (sections.length - 1),
+//         duration: 0.1,
+//         delay: 0.1,
+//         ease: "power1.inOut"
+//       },
+//       // base vertical scrolling on how wide the container is so it feels more natural.
+//       end: "+=3500"
+//     }
+//   });
+
+//! SECTION CARD
+
+let working=[
+    {img: './media/section2/static.jpeg', title: 'STATIC', price: '500'},
+    {img: './media/section2/portfolio.jpeg', title: 'PORTFOLIO', price: '1000'},
+    {img: './media/section2/e-commerce.jpeg', title: 'E-COMMERCE', price: '1500'},
+];
+
+let cardWrapper= document.querySelector('#card-wrapper');
+
+working.forEach((work)=>{
+    let div= document.createElement('div');
+    div.classList.add('col-12', 'col-md-4', 'h-100');
+    div.innerHTML= `
+        <div class="s2-card-container">
+            <div class="card-custom p-4">
+                <div class="h-50 w-100">
+                    <img class="img-fluid card-img-custom" src="${work.img}" alt="">
+                </div>
+                <div class="card-body-custom my-3 w-100">
+                    <h4>${work.title}</h4>
+                    <p>${work.price}$</p>
+                </div>
+                <button class="btn-card"><a class="no-decoration" href="#contact">BUY</a></button>
+            </div>
+        </div>
+    `
+    cardWrapper.appendChild(div);
+
+})
+
+//animazione card
+let cards = document.querySelectorAll('.card-custom');
+
+cards.forEach((card, index) =>{
+    card.setAttribute("data-aos","flip-left");
+    // card.setAttribute("data-aos","zoom-in"); --> alternative animation 
+    card.setAttribute("data-aos-duration","1000");
+    if(index==1){
+        card.setAttribute("data-aos-delay", "200");
+    }else if (index==2){
+        card.setAttribute("data-aos-delay", "300");
+    }
+});
+
+
+//! SECTION FEEDBACK AND SKILLS
+//Feedback
+
+//Feedback card dinamiche
+let users=[
+    {img: 'user1', name: 'Daniele', work: 'Medico', feedback: 'Fantastico lavoro!!!', social:'instagram'},
+    {img: 'user2', name: 'Rebecca', work: 'Ingegnere', feedback: 'Prezzo troppo basso per le capacità che dimostra...', social:'linkedin'},
+    {img: 'user3', name: 'Anna', work: 'Avvocato', feedback: 'Troppo bravo e gentile, lo consiglio a tutti.', social:'youtube'},
+    {img: 'user4', name: 'Andrea', work: 'Insegnante', feedback: 'Il sito è stato fatto esattamente come lo volevo', social:'twitter'},
+    {img: 'user5', name: 'Gabriele', work: 'Psicologo', feedback: 'Il migliore per realizzare il vostro sito personale', social:'tiktok'},  
+];
+
+let feedSlider= document.querySelector('#feedSlider');
+
+users.forEach((user)=>{
+    let div= document.createElement('div');
+    div.classList.add('feed');
+    div.innerHTML= `
+        <div class="d-flex justify-content-center rounded-5 w-100 my-4">
+            <div class="feed-container-img">
+                <div class="feed-img-${user.img} ps-2"></div>
+            </div>
+            <div class="feed-name">
+                <h5 class="m-0">${user.name}</h5>
+                <h6 class="fst-italic">${user.work}</h6>
+            </div>
+            <div class="feed-container-icon">
+                <i class="fa-brands fa-2xl fa-${user.social}"></i>
+            </div>
+        </div>
+        <div class="feed-text px-3 pt-4">
+            <p>${user.feedback}</p>
+        </div>
+    `
+    feedSlider.appendChild(div);
+})
+
+
+//Feedback animazione
+const feeds = document.querySelectorAll(".feed");
+const offset = 20;
+
+gsap.set(feeds, {
+  x: (index) => offset * index,
+  y: (index) => -offset * index,
+  zIndex: (index) => feeds.length - index,
+});
+
+
+function diagonalLoop(feeds) {
+    let totalFeeds = feeds.length;
+    // console.log(totalFeeds);
+
+    let currentFeed = 0;
+  
+    function updatePositions() {
+      for (let i = 0; i < totalFeeds; i++) {
+        let feedIndex = (currentFeed + i) % totalFeeds;
+        let feed = feeds[feedIndex];
+        gsap.to(feed, {
+          x: offset * i,
+          y: -offset * i,
+          zIndex: totalFeeds - i,
+          scale: 1,
+          opacity: 1
+        });
+      }
+    }
+    function moveToNext() {
+      const movingFeed = feeds[currentFeed];
+  
+      gsap.to(movingFeed, {
+        // scale: 0.5,
+        // y: "+=100",
+        // opacity: 0,
+        // duration: 0.1,
+        onComplete: () => {
+          currentFeed = (currentFeed + 1) % totalFeeds;
+        //   console.log(currentFeed);
+          updatePositions();
+  
+          gsap.to(movingFeed, {
+            scale: 1,
+            y: "-=50",
+            opacity: 0,
+            duration: 0.2,
+          });
+        }
+      });
+    }
+  
+    setInterval(moveToNext, 2000);
+  
+    updatePositions();
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+      diagonalLoop(feeds);
+  });
+
+//Skills animazione
+gsap?.from('.bar-text >span',{
+    scrollTrigger:'.bar-text',
+    left:0,
+    duration:3,
+    ease:'power2.out',
+    stagger: 0.1,
+})
+gsap?.from('.bar >span',{
+    scrollTrigger:'.bar',
+    width:'0px',
+    duration:3,
+    ease:'power2.out',
+    stagger: 0.1,
+})
+
+
+
+//Inizializzazione AOS
+// AOS.init();
---
+++ laravel_portfolio/resources/views/index.blade.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio demo</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body>
+    <!-- navbar  -->
+    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand nav-link link-color click-color" href="#">Brand</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav m-auto">
+            <a class="nav-link link-color no-decoration click-color"  aria-current="page" href="#">Home</a>
+            <a class="nav-link link-color no-decoration click-color"  href="#aboutMe">About Me</a>
+            <a class="nav-link link-color no-decoration click-color"  href="#service">Service</a>
+            <a class="nav-link link-color no-decoration click-color"  href="#skills">Skills</a>
+            <a class="nav-link link-color no-decoration click-color"  href="#contact">Contact</a>
+          </div>
+        </div>
+        <div>
+          <a class="link-color no-decoration mx-2" href="#"><i class="fa-brands fa-brands-navbar fa-instagram "></i></a>
+          <a class="link-color no-decoration mx-2" href="#"><i class="fa-brands fa-brands-navbar fa-youtube"></i></a>
+          <a class="link-color no-decoration mx-2" href="#"><i class="fa-brands fa-brands-navbar fa-linkedin"></i></a>
+        </div>
+      </div>
+    </nav>
+    <!-- header -->
+    <header>
+        <div class="container vh-100"> 
+            <div class="row h-100 align-items-center justify-content-center">
+                <div class="col-8">
+                  <div class="banner-hero d-flex justify-content-center align-items-center flex-column position-relative">
+                    <div class="banner-img">
+                      <!-- <img class="img-fluid" src="./media/hero/banner-img.jpeg" alt=""> -->
+                    </div>
+                    <div class="banner-text">
+                      <h1>WEB DEVELOPER</h1>
+                      <h2>Giovanni Rosario Mauro</h2>
+                    </div>
+                    <i class=" p-2 fa-solid fa-arrow-right arrow-hero"></i>
+                  </div>
+                </div>
+                <div class="col-4 super-container-logo-hero">
+                  <div class="container container-logo-hero">
+                    <div class="row">
+                      <!-- first record  -->
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/Angular Icon Logo PNG Vector (AI) Free Download.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/Javascript Logo (JS _ 01) - PNG Logo Vector Brand Downloads (SVG, EPS).png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/C Programming Language Logo, HD Png Download , Transparent Png Image - PNGitem.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/Node_js Logo PNG Vector (SVG) Free Download.png" alt="">
+                      </div>
+                      <!-- second record  -->
+                      <div class="col-3">
+                        <img class="logo_pro_custom my-4 " src="./media/hero/CSS3 Logo PNG vector in SVG, PDF, AI, CDR format.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom my-4 " src="./media/hero/Java Programming Icon PNG Images,  Java, Programming, Icon PNG Transparent Background - Pngtree.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom my-4 " src="./media/hero/bootstrap-logo.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom my-4" src="./media/hero/Top 20 famous logos designed in Orange.png" alt="">
+                      </div>
+                      <!-- third record  -->
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/gsap_logo_before_after.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/Laravel Logo PNG Vector (SVG) Free Download.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom" src="./media/hero/React Logo PNG Vector (SVG) Free Download.png" alt="">
+                      </div>
+                      <div class="col-3">
+                        <img class="logo_pro_custom mt-4" src="./media/hero/PHP-logo.svg.png" alt="">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- section 1 -->
+    <section id="aboutMe" class="container-fluid s1">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 col-md-6 h-100 p-0">
+          <div class=" h-100 d-flex justify-content-end align-items-center">
+            <div class="about-img " data-aos="fade-right" data-aos-duration="2000">
+            </div>
+          </div>
+          
+        </div>
+        <div class="col-12 col-md-6 p-0">
+          <div class="about-text fst-italic" data-aos="fade-left" data-aos-duration="2000">
+            <h3 class="pt-3 display-3 text-center">About Me</h3>
+            <p class="px-4 pb-4">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. non similique dolor dolores cumq voluptatibus eaque nulla, quibusdam perspiciatis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem consequatur perspiciatis, incidunt cum, saepe ex expedita architecto facilis illo consectetur enim accusamus error inventore, adipisci quisquam explicabo minus at minima! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vitae numquam recusandae ut eius aliquid repellendus doloribus ea ad placeat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores, mollitia illum nostrum ipsam hic et quae reprehenderit molestias laudantium corporis! Voluptatem, vitae.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- section 2  -->
+    <section id="service" class="container s2">
+        <div id="card-wrapper" class="row align-items-center h-100 row-s2-custom">
+          <div class="col-12 text-center" data-aos="fade-down" data-aos-duration="1000">
+            <h3 class="display-3 mt-5">Service</h3>
+          </div>
+        </div>
+    </section>
+    <!-- section 3  -->
+    <section id="skills" class="container-fluid s3">
+      <div class="row h-100 ">
+        <div class="col-12 col-md-6 text-center">
+          <div class="d-flex flex-column align-items-center h-100">
+            <h3 class="display-3 pt-5 mt-5" data-aos="fade-right" data-aos-duration="1000">Feedback</h3>
+            <div class="feed-wrapper" data-aos="fade-right" data-aos-duration="1000">
+              <div id="feedSlider" class="feed-slider">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 text-center">
+          <div class="d-flex flex-column align-items-center h-100">
+            <h3 class="display-3 pt-5 mt-5" data-aos="fade-left" data-aos-duration="1000">Skills</h3>
+            <div class="container-skills" data-aos="fade-left" data-aos-duration="1000">
+              <div class="bar-container">
+                <h4 class="bar-text">HTML <span class="bar-percent-1">90%</span></h4>
+                <div class="bar"><span class="bar-percent1"></span></div>
+              </div>
+              <div class="bar-container my-3">
+                <h4 class="bar-text">CSS <span class="bar-percent-2">80%</span></h4>
+                <div class="bar"><span class="bar-percent2"></span></div>
+              </div>
+              <div class="bar-container">
+                <h4 class="bar-text">JAVASCRIPT <span class="bar-percent-3">70%</span></h4>
+                <div class="bar"><span class="bar-percent3"></span></div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="contact" class="container s4">
+      <div class="row h-100">
+        <div class="col-12 title-contact">
+          <h3 class="display-3" data-aos="fade-down" data-aos-duration="1000">Contact Me</h3>
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+          <form action="" class="form-container">
+            <label for="name" data-aos="fade-right" data-aos-duration="1000">Name</label>
+            <input class="input-form" id="name" name="name" type="text" data-aos="fade-left" data-aos-duration="1000">
+            <label for="email" data-aos="fade-right" data-aos-duration="1000">Email</label>
+            <input class="input-form" id="email" name="email" type="email" data-aos="fade-left" data-aos-duration="1000">
+            <label for="message" data-aos="fade-right" data-aos-duration="1000">Message</label>
+            <textarea class="input-form" name="message" id="message" cols="30" rows="10" data-aos="fade-left" data-aos-duration="1000"></textarea>
+            <div class="w-100 d-flex justify-content-center">
+              <button class="btn-form" data-aos="fade-up" data-aos-duration="1000"><a class="no-decoration" href="#">Send</a></button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+    <footer class="foot container-fluid">
+      <div class="row justify-content-center align-items-center h-100">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+          <p class="text-footer text-cente p-0 m-0"> @ 2035 by Giovanni Rosario Mauro</p>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
